b5be7aa8cf4c9c8f9c7b02835bd4a6934562d8b7
ivegalag <ivegalag@everis.com>
2020-12-23 09:46:19 +0100

    Fix

--- src/main/java/Everis/everisdarmytasksms/TaskController.java
+++ src/main/java/Everis/everisdarmytasksms/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 	@Autowired
 	private TaskService taskService;
@@ -28,13 +30,10 @@ public class TaskController {
 	}
 	
 	@DeleteMapping(value="/{id}")
-	public String deleteTask(@PathVariable (name="id") Long id) {
+	public void deleteTask(@PathVariable (name="id") Long id) {
 		if(taskService.getTask(id)!= null) {
 			taskService.deleteById(id);
-			return "Task Delete Successfully";
-		} else {
-			return "Not Task With This ID";
-		}
+		} 
 	}
 
 	 @GetMapping("/{id}")
